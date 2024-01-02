2e52d89b91b7783d39ccb80c8750983b79f398db
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2024-01-01 23:04:53 -0800

    Almost done, just need to finish the select element

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-const name = "Abdulrahman"
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+
+//Functions
+const addTodo = (event) => {
+    //prevent form from submitting
+    event.preventDefault();
+
+    //Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    
+    //Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //completed button
+    const completedButton = document.createElement('button');
+    completedButton.classList.add('complete-btn');
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todoDiv.appendChild(completedButton);
+
+    //uncompleted button
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('trash-btn');
+    trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    todoDiv.appendChild(trashButton);
+
+    //append to list
+    todoList.appendChild(todoDiv);
+
+    //clear todo input value
+    todoInput.value = "";
+};
+
+const deleteCheck = (event) => {
+    const item = event.target
+    //delete TODO
+    if(item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        //animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+
+    //check mark
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+};
+
+
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
